f0775a1cbedd8b333fcfaf6d1023f02c984bb014
aswinthottilpalam <aswinp264@gmail.com>
2022-03-27 23:42:42 +0530

    Address bokk practice problem

--- AddressBookPracticeProblem/Program.cs
+++ AddressBookPracticeProblem/Program.cs
@@ -6,7 +6,7 @@ namespace AddressBookPracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to address book system");
         }
     }
 }
